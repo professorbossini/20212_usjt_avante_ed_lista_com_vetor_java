ec141f4c47bbdd8bbd52d9dd7500515791a051a0
Rodrigo Bossini <31605875+professorbossini@users.noreply.github.com>
2021-09-02 01:03:19 +0000

    Implementamos os métodos de redimensionamento e exibição.

--- Main.java
+++ Main.java
@@ -1,3 +1,5 @@
+import java.util.Random;
+
 class Lista{
   int [] elementos;
   int qtde;
@@ -8,20 +10,54 @@ class Lista{
       cap = 4;
   }
 
-  boolean estaCheia(){
+  boolean estaCheia (){
     return qtde >= cap;
     //return qtde == cap ? true : false;
   }
 
+ 
+
+  void aumentar (){
+    //alocar um novo vetor com o dobro do tamanho
+    int [] aux = new int[cap * 2];
+    
+    //copiar os elementos para o novo vetor
+    for (int i = 0; i < qtde; i++){
+      aux[i] = elementos[i];
+    }
+    //dobrar a capacidade
+    cap *= 2;
+    //ajustar a variável de referência
+    elementos = aux;
+  }
+
   void inserir (int e){
+    if (estaCheia())
+      aumentar();
     elementos[qtde] = e;
     qtde++;
+  }
+
+  void exibir(){
+    System.out.printf ("*******************\n");
+    System.out.printf ("Qtde: %d\n", qtde);
+    System.out.printf ("Cap: %d\n", cap);
+    for (int i = 0; i < this.qtde; i++){
+      System.out.printf ("%d ", this.elementos[i]);
+    }
+    System.out.printf ("\n");
   }
 }
 
 
 class Main {
   public static void main(String[] args) {
-    System.out.println("Hello world!");
+    Random gerador = new Random();
+    Lista umaLista = new Lista();
+    for (int i = 0; i < 100; i++){
+      int dado = gerador.nextInt(20) + 1;
+      umaLista.inserir(dado);
+      umaLista.exibir();
+    }
   }
 }
